db50ded1b1af117538b5ff6cc82a37b69a39136c
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-11 15:19:30 +0500

    Update Added Class Quiz contract class

---
+++ app/src/main/java/com/example/usmankhan/quizapplication/QuizContract.java
@@ -0,0 +1,20 @@
+package com.example.usmankhan.quizapplication;
+
+import android.provider.BaseColumns;
+
+public final class QuizContract {
+
+    private QuizContract(){}
+
+    public static class QuestionTable implements BaseColumns
+    {
+        public static final String TABLE_NAME="quiz_question";
+        public static final String COLUMN_NAME="Question";
+        public static final String COLUMN_OPTION1="Option1";
+        public static final String COLUMN_OPTION2="Option2";
+        public static final String COLUMN_OPTION3="Option3";
+        public static final String COLUMN_OPTION4="Option4";
+        public static final String COLUMN_ANSNO="AnswerNr";
+
+    }
+}
