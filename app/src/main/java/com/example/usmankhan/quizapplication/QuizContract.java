a2f63dbae5a540de7b3eabc1af97889acd260d74
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-20 15:54:01 +0500

    Update added New Class QuizCategory and implement the interface

--- app/src/main/java/com/example/usmankhan/quizapplication/QuizContract.java
+++ app/src/main/java/com/example/usmankhan/quizapplication/QuizContract.java
@@ -6,6 +6,16 @@ public final class QuizContract {
 
     private QuizContract(){}
 
+
+    public static class QuizCategoryTable implements BaseColumns {
+        public static final String TABLE_NAME = "quiz_categories";
+        public static final String COLUMN_NAME = "name";
+
+    }
+
+
+
+
     public static class QuestionTable implements BaseColumns
     {
         public static final String TABLE_NAME="quiz_question";
@@ -16,6 +26,7 @@ public final class QuizContract {
         public static final String COLUMN_OPTION4="Option4";
         public static final String COLUMN_ANSNO="AnswerNr";
         public static final String COLUMN_Difficulty = "difficulty";
+        public static final String COLUMN_QUIZCATEGORY_ID = "quizcategory_id";
 
     }
 }
